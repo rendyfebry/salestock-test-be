9f8d50be9447b102c6d5830f1a8c169e51d59901
Rendy Febry <rendyfebry@gmail.com>
2018-07-17 16:50:25 +0700

    Refactor Product controller from Static data to database

--- controllers/product.js
+++ controllers/product.js
@@ -1,261 +1,41 @@
+const db = require('../models/db')
+
+const products = db.get('product')
+
 const Product = {}
 
-const productData = [
-	{
-		name: 'Aridatha Plain Drapped Midi Dress',
-		slug: 'aridatha-plain-drapped-midi-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/f1f19d0b4f413388031408e47de8b0b8.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-	{
-		name: 'Zalifa Brukat Flare Mini Dress',
-		slug: 'zalifa-brukat-flare-mini-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/0c0bf8024b8b627db756ddb1f050bee1.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-	{
-		name: 'Gyamaru Lace Drapped Mini Dress',
-		slug: 'gyamaru-lace-drapped-mini-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/00cc30c2bcb967a9dbb87fe6f32e2e65.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-	{
-		name: 'Helsexia Plain V-Neck Mini Dress',
-		slug: 'helsexia-plain-v-neck-mini-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/608d4411aad8d3eef5f35235a74bd0a7.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-	{
-		name: 'Foyuilzha Brukat Cape Mini Dress',
-		slug: 'foyuilzha-brukat-cape-mini-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/11dfe4b4a43e83e6d49e8b2eea167491.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-	{
-		name: 'Elhepone Flowery Cold Shoulder Mini Dress',
-		slug: 'elhepone-flowery-cold-shoulder-mini-dress',
-		price: 155000,
-		category: 'dress',
-		images: [
-			'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/fbd2b5a3a1e1bdbafe0a42a0c19f1509.jpg',
-		],
-		colors: ['Black', 'Broken White', 'Grey', 'Navy', 'Red', 'Tosca'],
-		sizes: [
-			{
-				name: 'S',
-				lingkarDada: 90,
-				panjangLengan: 11,
-				panjang: 90,
-				lingkarPinggang: 94,
-			},
-			{
-				name: 'M',
-				lingkarDada: 94,
-				panjangLengan: 12,
-				panjang: 90,
-				lingkarPinggang: 98,
-			},
-			{
-				name: 'L',
-				lingkarDada: 100,
-				panjangLengan: 12,
-				panjang: 92,
-				lingkarPinggang: 106,
-			},
-			{
-				name: 'XL',
-				lingkarDada: 110,
-				panjangLengan: 13,
-				panjang: 95,
-				lingkarPinggang: 118,
-			},
-		],
-	},
-]
-
 Product.getAll = async (req, res, next) => {
-	const productList = productData.map(product => {
-		delete product.colors
-		delete product.sizes
-		return product
-	})
+	const page = parseInt(req.query.page) || 1
+	const limit = parseInt(req.query.perPage) || 3
+	const skip = (page - 1) * limit
 
-	const response = { error: 0, message: 'success', data: productList }
-	return res.send(response)
+	const productList = await products.find(
+		{},
+		{
+			fields: { _id: 0, colors: 0, sizes: 0 },
+			skip: skip,
+			limit: limit,
+		},
+	)
+
+	const productTotal = await products.count()
+
+	if (productList && productList.length > 0) {
+		const response = {
+			error: 0,
+			message: 'success',
+			data: productList,
+			meta: { page: page, perPage: limit, total: productTotal },
+		}
+		return res.send(response)
+	}
+
+	const response = { error: 404, message: 'Not Found', data: [] }
+	return res.status(404).send(response)
 }
 
 Product.getSingleBySlug = async (req, res, next) => {
-	const selectedProduct = productData.find(i => i.slug === req.params.slug)
+	const selectedProduct = await products.findOne({ slug: req.params.slug }, { _id: 0 })
 
 	if (selectedProduct) {
 		const response = { error: 0, message: 'success', data: selectedProduct }
